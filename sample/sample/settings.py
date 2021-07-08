850abe8661285cffa296e20fc3cabf5b8738b9c9
ゆずき <k.saito70329@gmail.com>
2021-07-08 18:54:42 +0900

    [update] setting environ django sercret key

--- sample/sample/settings.py
+++ sample/sample/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j^6$7@^qdgqlnu*d*#(3@v&3qqri1ljs_m-gyz^f*icr&p+$y-'
+SECRET_KEY = env('DJANGO_SAMPLE_SERCRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- user_api/user_api/settings.py
+++ user_api/user_api/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p_awv5v9ris_r%!skvr1j(zlibf*f0g4f=8hr%^q0=de$)&c=y'
+SECRET_KEY = env('DJANGO_USER_API_SERCRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
